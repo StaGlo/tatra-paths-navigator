3253342f8f6613622a1d027532e24d8d9841678e
StaGlo <stanley.glowczewski@gmail.com>
2024-04-20 14:11:45 +0200

    Improve PatDetail screen Add scrollability

--- app/src/main/java/com/example/touristpath/ui/screen/PathDetail.kt
+++ app/src/main/java/com/example/touristpath/ui/screen/PathDetail.kt
@@ -8,19 +8,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MonotonicFrameClock
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.touristpath.data.PathObject
 import com.example.touristpath.data.pathList
@@ -37,19 +36,26 @@ fun PathDetail(
     val (showStopper, setShowStopper) = remember { mutableStateOf(false) }
 
     Column(
-        modifier = modifier.padding(16.dp),
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             style = MaterialTheme.typography.headlineMedium,
             text = path.title,
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
                 .padding(bottom = 24.dp)
         )
 
         Text(
-            style = MaterialTheme.typography.bodyMedium, text = path.description
+            style = MaterialTheme.typography.bodyMedium,
+            text = path.description,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 24.dp)
         )
 
         if (showStopper) {
@@ -61,7 +67,9 @@ fun PathDetail(
         Row(
             horizontalArrangement = Arrangement.SpaceAround,
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
         ) {
             Button(onClick = {
                 Log.d("PathDetail", "ShowStopper: $showStopper")
@@ -97,6 +105,9 @@ fun PathDetail(
 @Composable
 fun PathDetailPreview() {
     TouristPathTheme {
-        PathDetail(pathList.first())  // Pass first item as a mock detail view
+        PathDetail(
+            pathList.first(),
+            isLargeScreen = false,
+        )  // Pass first item as a mock detail view
     }
 }
